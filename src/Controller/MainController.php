47c033c69a30338f942320f4259a01de65af8172
Hugo Pérez Candal <pehugo2@gmail.com>
2021-02-01 12:43:26 +0100

    Created base page twig and css

---
+++ src/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController {
+
+  /**
+   * @Route("/",
+   * name="index")
+   */
+  public function index():Response {
+    return $this->render('index.html.twig');
+  }
+
+
+
+}
