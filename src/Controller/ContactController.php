a7ca52be936f7651244ae02f227295d4e502231a
Hugo Perez <pehugo2@gmail.com>
2021-02-02 17:45:53 +0100

    Added new Contact Route and form creator

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
@@ -14,6 +16,29 @@ class ContactController extends AbstractController
      */
     public function general($type): Response
     {
-        return $this->render('contact/list.html.twig', ['type' => $type]);
+        $croppedType = substr($type, 0, 2);
+
+        $contacts = $this->getDoctrine()
+        ->getRepository(Contact::class)
+        ->findBy(['type' => $croppedType]);
+
+        return $this->render('Contact/list.html.twig', [
+            'type' => $type,
+            'list' => $contacts
+        ]);
     }
+
+    /**
+     * @Route("/new", 
+     * name="new")
+     */
+    public function newContact(): Response
+    {
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+        
+        return $this->render('Contact/new.html.twig', ['form' => $form->createView()]);
+    }
+    
 }
